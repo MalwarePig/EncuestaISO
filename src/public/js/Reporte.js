c8b569256eac82e08b44923c6d9384d90c85580e
MalwarePig <segio_lcc92@hotmail.com>
2020-09-09 23:39:12 -0500

    Reporte

--- src/public/js/Reporte.js
+++ src/public/js/Reporte.js
@@ -1,192 +1,112 @@
 //=========================================== BUSCAR TRABAJADORES POR NUMERO DE NOMINA =================================================//
 function Tabla() {
-    $.ajax({
-        url: '/listaMaquinas/' + familia + '',
-        success: function (maquinas) {
-            for (let i = listMaquina.options.length; i >= 0; i--) { //Borrar elementos option de select
-                listMaquina.remove(i);
-            }
-            for (var i = 0; i < maquinas.length; i++) { //Agregar nuevos options del select
+    //Se obtiene el nodo
+    var Nodo = document.getElementById("Cat1");
+    //se crea texto para el nodo
+    var newText = document.createTextNode(564);
+    //se inserta el valor al nodo
+    Nodo.appendChild(newText);
 
-                var option = document.createElement("option");
-                option.text = maquinas[i].Nombre;
-                listMaquina.add(option);
-            }
-        }//Funcion success
-    });//Ajax
-    
+    var Nodo = document.getElementById("R1");
+    var newText = document.createTextNode(564);
+    Nodo.appendChild(newText);
+    //////////////////////////////////////////////////
+    var Nodo = document.getElementById("Cat2");
+    var newText = document.createTextNode(95);
+    Nodo.appendChild(newText);
+
+    var Nodo = document.getElementById("R2");
+    var newText = document.createTextNode(95);
+    Nodo.appendChild(newText);
+    //////////////////////////////////////////////////
+    var Nodo = document.getElementById("Cat3");
+    var newText = document.createTextNode(92);
+    Nodo.appendChild(newText);
+
+    var Nodo = document.getElementById("R3");
+    var newText = document.createTextNode(92);
+    Nodo.appendChild(newText);
+
+    //////////////////////////////////////////////////
+    var Nodo = document.getElementById("Cat4");
+    var newText = document.createTextNode(92);
+    Nodo.appendChild(newText);
+
+    var Nodo = document.getElementById("Dom2");
+    var newText = document.createTextNode(92);
+    Nodo.appendChild(newText);
+
+    var Nodo = document.getElementById("Dim2");
+    var newText = document.createTextNode(92);
+    Nodo.appendChild(newText);
+
+    var Nodo = document.getElementById("R4");
+    var newText = document.createTextNode(15);
+    Nodo.appendChild(newText);
+    /////////////////////////////////////////////////
+    var Nodo = document.getElementById("Dim3");
+    var newText = document.createTextNode(92);
+    Nodo.appendChild(newText);
+
+    var Nodo = document.getElementById("R5");
+    var newText = document.createTextNode(20);
+    Nodo.appendChild(newText);
+    /////////////////////////////////////////////////
+    var Nodo = document.getElementById("Dim4");
+    var newText = document.createTextNode(92);
+    Nodo.appendChild(newText);
+
+    var Nodo = document.getElementById("R6");
+    var newText = document.createTextNode(25);
+    Nodo.appendChild(newText);
+    /////////////////////////////////////////////////
+    var Nodo = document.getElementById("Dim5");
+    var newText = document.createTextNode(92);
+    Nodo.appendChild(newText);
+
+    var Nodo = document.getElementById("R7");
+    var newText = document.createTextNode(10);
+    Nodo.appendChild(newText);
+    /////////////////////////////////////////////////
+    var Nodo = document.getElementById("Dim6");
+    var newText = document.createTextNode(92);
+    Nodo.appendChild(newText);
+
+    var Nodo = document.getElementById("R8");
+    var newText = document.createTextNode(20);
+    Nodo.appendChild(newText);
+    /////////////////////////////////////////////////
+    var Nodo = document.getElementById("Dim7");
+    var newText = document.createTextNode(92);
+    Nodo.appendChild(newText);
+
+    var Nodo = document.getElementById("R9");
+    var newText = document.createTextNode(30);
+    Nodo.appendChild(newText);
+    ////////////////////////////////////////////////
+    var Nodo = document.getElementById("Dom3");
+    var newText = document.createTextNode(92);
+    Nodo.appendChild(newText);
+
+    var Nodo = document.getElementById("Dim8");
+    var newText = document.createTextNode(92);
+    Nodo.appendChild(newText);
+
+    var Nodo = document.getElementById("R10");
+    var newText = document.createTextNode(15);
+    Nodo.appendChild(newText);
 }
-
-function Fecha() {
-    var tabla = document.getElementById('Registros').getElementsByTagName('tbody')[0];
-    var total = tabla.rows.length//Total de filasa
-    for (var j = 0; j < total; j++) {//filas
-        let Fecha =  new Date(tabla.rows[j].cells[5].childNodes[0].nodeValue);
-        console.log('Fecha tabla ' + Fecha);
-        var dd = Fecha.getDate();
-        var mm = Fecha.getMonth() + 1;
-        var yyyy = Fecha.getFullYear();
-        var HH = Fecha.getHours();
-        var mi = Fecha.getMinutes();
-
-        if (dd < 10) {
-            dd = '0' + dd;
+    /*$.ajax({
+    url: '/listaMaquinas/' + familia + '',
+    success: function (maquinas) {
+        for (let i = listMaquina.options.length; i >= 0; i--) { //Borrar elementos option de select
+            listMaquina.remove(i);
         }
-        if (mm < 10) {
-            mm = '0' + mm;
+        for (var i = 0; i < maquinas.length; i++) { //Agregar nuevos options del select
+
+            var option = document.createElement("option");
+            option.text = maquinas[i].Nombre;
+            listMaquina.add(option);
         }
-        var today = dd + '/' + mm + '/' + yyyy;
-        document.getElementById("Fecha" + j).innerHTML = today;
-        console.log('Fecha Funcion ' + today);
-    }//fin filas
-}
-
-function Color() {
-    try {
-        var tabla = document.getElementById('Registros').getElementsByTagName('tbody')[0];
-        var total = tabla.rows.length//Total de filasa
-        let Condicion = "";
-        for (var j = 0; j < total; j++) {//filas
-            Condicion = tabla.rows[j].cells[0].childNodes[0].nodeValue;
-            if (Condicion == "Aceptado") {
-                document.getElementById("Fila" + j).style.backgroundColor = " #b5d687 "; //Verde
-            } else if (Condicion == "Negado") {
-                document.getElementById("Fila" + j).style.backgroundColor = "#e75c5c"; //Rojo
-            } else {
-                document.getElementById("Fila" + j).style.backgroundColor = "#ee8018 "; //Naranja
-            }
-        }//fin filas
-    } catch (err) {
-        console.log(err);
-    }
-}
-
-function Modal() {
-    $("#ModalSection").modal();
-    //document.getElementById("Formualario").reset();
-    //setTimeout("location.reload()", 3000);
-}
-
-function Excel() {
-    var table = document.getElementById("Registros");
-    var total = table.rows.length;//Total de filas
-    console.log(total);
-    var tabla = document.getElementById("Registros");
-    var wb = XLSX.utils.table_to_book(tabla, { sheet: "Reporte" });
-    var wbout = XLSX.write(wb, { bookType: 'xlsx', bookSST: true, type: 'binary' });
-    function s2ab(s) {
-        var buf = new ArrayBuffer(s.length);
-        var view = new Uint8Array(buf);
-        for (var i = 0; i < s.length; i++) view[i] = s.charCodeAt(i) & 0xFF;
-        return buf;
-    }
-    saveAs(new Blob([s2ab(wbout)], { type: "application/octet-stream" }), 'Reporte_Covid.xlsx');
-}
-
-function Pendientes() {
-
-    var url = window.location.href;
-    var Condicion = url.indexOf("ReporteM");
-    var Planta = "";
-    var tabla = document.getElementById("Registros");
-    var total = tabla.rows.length//Total de filas
-    let Area = document.getElementById("Area").value;
-    if (Condicion > 0) {
-        Planta = "Morelos";
-        $.ajax({
-            url: '/PendientesMorelos/' + Area,
-            success: function (data) {
-
-                for (var j = 0; j < data.length; j++) {
-                    var existencia = false;
-                    var Nombre = data[j].Nombre;
-                    var Nomina = data[j].Nomina;
-                    var Planta = data[j].Planta;
-                    for (var i = 1; i <= total - 1; i++) {
-                        if (tabla.rows[i].cells[2].childNodes[0].nodeValue == Nomina) {
-                            existencia = true;
-                        }
-                    }// i Tabla
-                    if (existencia == false) {
-                        //alert('falta' + Nomina)
-                        Arreglo = ['Sin registro', Nombre, Nomina, Planta, '-', '-'];
-                        var Tabla = document.getElementById('Registros').getElementsByTagName('tbody')[0];
-                        // inserta una fila al final de la tabla
-                        var newRow = Tabla.insertRow(Tabla.rows.length);
-                        for (var x = 0; x < Arreglo.length; x++) {
-                            // inserta una celda en el indice 0
-                            var newCell = newRow.insertCell(x);
-                            newRow.setAttribute("id", "Fila" + j);//se asigna id al incrementar cada fila +1 para contar el encabezado
-                            // adjuntar el texto al nodo
-                            var newText = document.createTextNode(Arreglo[x]);
-                            if (x == 5) {
-                                newCell.setAttribute("id", "Fecha" + i);//se asigna id al incrementar cada fila +1 para contar el encabezado
-                            }
-                            newCell.appendChild(newText);
-                            newCell.style.backgroundColor = "#a9a5a4  "; //gris
-                        }
-                    }
-                }// j data
-            }
-        })
-    } else {
-        Planta = "Bravo"
-        $.ajax({
-            url: '/PendientesBravo/' + Area,
-            success: function (data) {
-
-                for (var j = 0; j < data.length; j++) {
-                    var existencia = false;
-                    var Nombre = data[j].Nombre;
-                    var Nomina = data[j].Nomina;
-                    var Planta = data[j].Planta;
-                    for (var i = 1; i <= total - 1; i++) {
-                        if (tabla.rows[i].cells[2].childNodes[0].nodeValue == Nomina) {
-                            existencia = true;
-                        }
-                    }// i Tabla
-                    if (existencia == false) {
-                        //alert('falta' + Nomina)
-                        Arreglo = ['Sin registro', Nombre, Nomina, Planta, '-', '-'];
-                        var Tabla = document.getElementById('Registros').getElementsByTagName('tbody')[0];
-                        // inserta una fila al final de la tabla
-                        var newRow = Tabla.insertRow(Tabla.rows.length);
-                        for (var x = 0; x < Arreglo.length; x++) {
-                            // inserta una celda en el indice 0
-                            var newCell = newRow.insertCell(x);
-                            newRow.setAttribute("id", "Fila" + j);//se asigna id al incrementar cada fila +1 para contar el encabezado
-                            // adjuntar el texto al nodo
-                            var newText = document.createTextNode(Arreglo[x]);
-                            if (x == 5) {
-                                newCell.setAttribute("id", "Fecha" + i);//se asigna id al incrementar cada fila +1 para contar el encabezado
-                            }
-                            newCell.appendChild(newText);
-                            newCell.style.backgroundColor = "#a9a5a4  "; //gris
-                        }
-                    }
-                }// j data
-            }
-        })
-    }
-    //alert("Pendientes: " + total + 'Nomina'+tabla.rows[1].cells[2].childNodes[0].nodeValue);
-}
-
-/*==========================================    FORMTATO DE FECHA    ==========================================================================
-===============================================================================================================================================*/
-function FormtatoFechas(Fecha) {
-    var today = new Date(Fecha);
-    var dd = today.getDate();
-    var mm = today.getMonth();
-    var yyyy = today.getFullYear();
-    var HH = today.getHours();
-    var mi = today.getMinutes();
-
-    if (dd < 10) {
-        dd = '0' + dd;
-    }
-    if (mm < 10) {
-        mm = '0' + mm;
-    }
-    var today = dd + '/' + mm + '/' + yyyy;
-    return today;
-}
+    }//Funcion success
+});//Ajax*/
